21664180c9bd4ab5cef260daabe94466bdaafa87
bidurbhurtel <bidurbhurtel02@gmail.com>
2023-01-07 12:54:53 -0800

    First set up of restAPI

---
+++ src/main/java/com/bbhurtel/springboot/learnrest/LearnRestApplication.java
@@ -0,0 +1,13 @@
+package com.bbhurtel.springboot.learnrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbhurtel/springboot/learnrest/LearnRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbhurtel.springboot.learnrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
