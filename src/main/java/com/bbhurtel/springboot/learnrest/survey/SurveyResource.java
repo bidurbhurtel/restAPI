6f3937da9e26f5865a799e51ae98c10cb574f988
bidurbhurtel <bidurbhurtel02@gmail.com>
2023-01-07 21:44:01 -0800

    Created Get, Post, Delete and Put for resources

--- src/main/java/com/bbhurtel/springboot/learnrest/survey/SurveyResource.java
+++ src/main/java/com/bbhurtel/springboot/learnrest/survey/SurveyResource.java
@@ -1,9 +1,17 @@
 package com.bbhurtel.springboot.learnrest.survey;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.net.URI;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class SurveyResource {
@@ -18,4 +26,57 @@ public class SurveyResource {
     public List<Survey> retrieveAllSurvey() {
         return surveyService.retrieveAllSurvey();
     }
+
+    @RequestMapping("/surveys/{surveyId}")
+    public Survey retriveSurveyById(@PathVariable String surveyId) {
+        Survey survey = surveyService.retriveSurveyById(surveyId);
+        if (survey == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return survey;
+    }
+
+    @RequestMapping("/surveys/{surveyId}/questions")
+    public List<Question> retrieveAllSurveyQuestions(@PathVariable String surveyId) {
+        List<Question> questions = surveyService.retrieveAllSurveyQuestions(surveyId);
+        if (questions == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return questions;
+    }
+
+    @RequestMapping("/surveys/{surveyId}/questions/{questionId}")
+    public Question retrieveSurveyQuestionsById(@PathVariable String surveyId, @PathVariable String questionId) {
+        Question question = surveyService.retrieveSurveyQuestionsById(surveyId, questionId);
+        if (question == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return question;
+    }
+
+    @RequestMapping(value = "/surveys/{surveyId}/questions", method = RequestMethod.POST)
+    public ResponseEntity<Object> addNewSurveyQuestions(@PathVariable String surveyId,
+            @RequestBody Question question) {
+        String questionId = surveyService.addNewSurveyQuestions(surveyId, question);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{questionId}").buildAndExpand(questionId)
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    // TODO : return question id instead of noContent()
+    @RequestMapping(value = "/surveys/{surveyId}/questions/{questionId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteSurveyQuestion(@PathVariable String surveyId,
+            @PathVariable String questionId) {
+        surveyService.deleteSurveyQuestion(surveyId, questionId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/surveys/{surveyId}/questions/{questionId}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateSurveyQuestion(@PathVariable String surveyId,
+            @PathVariable String questionId,
+            @RequestBody Question question) {
+        surveyService.updateSurveyQuestion(surveyId, questionId, question);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/bbhurtel/springboot/learnrest/survey/SurveyService.java
+++ src/main/java/com/bbhurtel/springboot/learnrest/survey/SurveyService.java
@@ -1,36 +1,102 @@
 package com.bbhurtel.springboot.learnrest.survey;
 
-import org.springframework.stereotype.Service;
-
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class SurveyService {
 
-    private static List<Survey> surveys = new ArrayList<>();
+        private static List<Survey> surveys = new ArrayList<>();
 
-    static {
-        Question question1 = new Question("Question1",
-                "Most Popular Cloud Platform Today", Arrays.asList(
-                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
-        Question question2 = new Question("Question2",
-                "Fastest Growing Cloud Platform", Arrays.asList(
-                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "Google Cloud");
-        Question question3 = new Question("Question3",
-                "Most Popular DevOps Tool", Arrays.asList(
-                "Kubernetes", "Docker", "Terraform", "Azure DevOps"), "Kubernetes");
+        static {
+                Question question1 = new Question("Question1",
+                                "Most Popular Cloud Platform Today", Arrays.asList(
+                                                "AWS", "Azure", "Google Cloud", "Oracle Cloud"),
+                                "AWS");
+                Question question2 = new Question("Question2",
+                                "Fastest Growing Cloud Platform", Arrays.asList(
+                                                "AWS", "Azure", "Google Cloud", "Oracle Cloud"),
+                                "Google Cloud");
+                Question question3 = new Question("Question3",
+                                "Most Popular DevOps Tool", Arrays.asList(
+                                                "Kubernetes", "Docker", "Terraform", "Azure DevOps"),
+                                "Kubernetes");
 
-        List<Question> questions = new ArrayList<>(Arrays.asList(question1,
-                question2, question3));
+                List<Question> questions = new ArrayList<>(Arrays.asList(question1,
+                                question2, question3));
 
-        Survey survey = new Survey("Survey1", "My Favorite Survey",
-                "Description of the Survey", questions);
-        surveys.add(survey);
-    }
+                Survey survey = new Survey("Survey1", "My Favorite Survey",
+                                "Description of the Survey", questions);
+                surveys.add(survey);
+        }
 
-    public List<Survey> retrieveAllSurvey(){
-        return surveys;
-    }
+        public List<Survey> retrieveAllSurvey() {
+                return surveys;
+        }
+
+        public Survey retriveSurveyById(String surveyId) {
+                Optional<Survey> survey = surveys.stream().filter(s -> s.getId().equals(surveyId)).findFirst();
+                if (survey.isEmpty()) {
+                        return null;
+                }
+                return survey.get();
+        }
+
+        public List<Question> retrieveAllSurveyQuestions(String surveyId) {
+                Optional<Survey> survey = surveys.stream().filter(s -> s.getId().equals(surveyId)).findFirst();
+                if (survey.isEmpty()) {
+                        return null;
+                }
+                return survey.get().getQuestions();
+        }
+
+        public Question retrieveSurveyQuestionsById(String surveyId, String questionId) {
+                List<Question> surveyQuestions = retrieveAllSurveyQuestions(surveyId);
+                if (surveyQuestions.isEmpty()) {
+                        return null;
+                }
+                Optional<Question> question = surveyQuestions.stream()
+                                .filter(q -> q.getId().equals(questionId)).findFirst();
+                if (question.isEmpty()) {
+                        return null;
+                }
+                return question.get();
+        }
+
+        public String generateRandomId() {
+                SecureRandom secureRandom = new SecureRandom();
+                String randomId = new BigInteger(32, secureRandom).toString();
+                return randomId;
+        }
+
+        public String addNewSurveyQuestions(String surveyId, Question question) {
+                List<Question> questions = retrieveAllSurveyQuestions(surveyId);
+                question.setId(generateRandomId());
+                questions.add(question);
+                return question.getId();
+        }
+
+        public String deleteSurveyQuestion(String surveyId, String questionId) {
+                List<Question> surveyQuestions = retrieveAllSurveyQuestions(surveyId);
+                if (surveyQuestions.isEmpty()) {
+                        return null;
+                }
+                boolean removed = surveyQuestions.removeIf(q -> q.getId().equals(questionId));
+                if (!removed) {
+                        return null;
+                }
+                return questionId;
+        }
+
+        public void updateSurveyQuestion(String surveyId, String questionId, Question question) {
+                List<Question> surveyQuestions = retrieveAllSurveyQuestions(surveyId);
+                surveyQuestions.removeIf(q -> q.getId().equals(questionId));
+                surveyQuestions.add(question);
+        }
 }
